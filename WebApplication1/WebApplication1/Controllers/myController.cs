fb1e5516fc5e332575aee53602bbb4acf20f71fe
KIST Student3 <student3@sddb.student>
2022-03-11 07:53:14 +0545

    Updated Database and Project

--- WebApplication1/WebApplication1/Controllers/myController.cs
+++ WebApplication1/WebApplication1/Controllers/myController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class myController : Controller
     {
         // GET: my
+        myEntities db = new myEntities();
         public ActionResult  Shaw()
         {
-            return View();
+            List<myDetail> data = db.myDetails.ToList();
+            return View(data);
         }
     }
 }
--- sql files/SQLQuery1.sql
+++ sql files/SQLQuery1.sql
@@ -11,3 +11,6 @@ contact bigint
 drop table myDetails;
 
 insert into mydetails values(1,'Anurag','baneshwor',9999999999);
+insert into mydetails values(2,'Rohan','NayaBasti',8888888888);
+insert into mydetails values(3,'Prashansa','Gothatar',7777777777);
+insert into mydetails values(4,'Kushal','Jyatha',6666666666);
